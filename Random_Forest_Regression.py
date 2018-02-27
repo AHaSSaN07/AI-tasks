1bbaa6d41e3fdf7723e597caa0dbbdb2c141c3be
Ahmed Hassan <enga.hassan60@gmail.com>
2018-02-27 18:23:15 +0200

    Update Random_Forest_Regression.py

--- Random_Forest_Regression.py
+++ Random_Forest_Regression.py
@@ -24,31 +24,39 @@ Y =data.iloc[:,8]
 
 #splitting into train and test sets 
 
-xtrain,xtest,ytrain,ytest =train_test_split(X,Y,test_size =.5,random_state =0)
+xtrain,xtest,ytrain,ytest =train_test_split(X,Y,test_size =.2,random_state =0)
 
 
-##applying linear regression (out>>2.16609908149)
-#linreg=LinearRegression()
-#linreg.fit(xtest,ytest)
-#result =linreg.predict(xtest)
+##applying linear regression (out->2.16609908149)
+linreg=LinearRegression()
+linreg.fit(xtrain,ytrain)
+result3 =linreg.predict(xtest)
 
 
 
 #applying Decision Trees
-#from sklearn.tree import tree
-#desction_tree =tree.DecisionTreeClassifier()
-#desction_tree.fit(xtest,ytest)
-#result =desction_tree.predict(xtest)
+from sklearn.tree import tree
+desction_tree =tree.DecisionTreeClassifier()
+desction_tree.fit(xtrain,ytrain)
+result2 =desction_tree.predict(xtest)
 
 
 #applying random Forest
+
 from sklearn.ensemble import RandomForestRegressor
-Regressor = RandomForestRegressor()
-Regressor.fit(xtest,ytest)
+Regressor = RandomForestRegressor(n_estimators =300,criterion ='mse')
+Regressor.fit(xtrain,ytrain)
 result =Regressor.predict(xtest)
 
-
-
 #error
-print(np.sqrt(mean_squared_error(ytest,result)))
+print("Random forest regression error:",np.sqrt(mean_squared_error(ytest,result)))
+print("linear regression error:",np.sqrt(mean_squared_error(ytest,result3)))
+print("Decision Trees error:",np.sqrt(mean_squared_error(ytest,result2)))
+
+
+
+#output
+#Random forest regression error: 2.07865245117
+#linear regression error: 2.17697609745
+#lDecision Trees error: 2.89984325595
 
