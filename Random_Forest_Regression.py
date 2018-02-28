f473b33fcf6fe90422f27d0623d3dd957dd61dbb
Ahmed Hassan <enga.hassan60@gmail.com>
2018-03-01 01:32:31 +0200

    Update Random_Forest_Regression.py

--- Random_Forest_Regression.py
+++ Random_Forest_Regression.py
@@ -16,6 +16,10 @@ data.columns = ['sex','length','diameter','height','whole weight','shucked weigh
 le = LabelEncoder()
 le.fit(data.sex)
 data.sex = le.transform(data.sex)
+#removing outliers 
+
+for j in range(1, len(data.columns)):
+    data = data[np.abs(data[data.columns[j]]-data[data.columns[j]].mean())<=(3*data[data.columns[j]].std())]
 
 #spliting into input and output
 
@@ -24,7 +28,7 @@ Y =data.iloc[:,8]
 
 #splitting into train and test sets 
 
-xtrain,xtest,ytrain,ytest =train_test_split(X,Y,test_size =.2,random_state =0)
+xtrain,xtest,ytrain,ytest =train_test_split(X,Y,test_size =.33,random_state =42)
 
 
 ##applying linear regression (out->2.16609908149)
@@ -44,24 +48,41 @@ result2 =desction_tree.predict(xtest)
 #applying random Forest
 
 from sklearn.ensemble import RandomForestRegressor
-Regressor = RandomForestRegressor(n_estimators =300,criterion ='mse')
+Regressor = RandomForestRegressor(n_estimators =500,criterion ='mse')
 Regressor.fit(xtrain,ytrain)
 result =Regressor.predict(xtest)
+
+##applying XGB
+#from xgboost.sklearn import XGBRegressor
+##
+#XGBoostRegressor = XGBRegressor()
+#XGBoostRegressor.fit(xtrain,ytrain)
+#XGBoostRegressorResult = XGBoostRegressor.predict(xtest)
+##
+
+############################################################
+from sklearn.ensemble import GradientBoostingRegressor
+
+gradientBoostingRegressor = GradientBoostingRegressor()
+gradientBoostingRegressor.fit(xtrain,ytrain)
+gradientBoostingRegressorResult = gradientBoostingRegressor.predict(xtest)
+
+
+
 
 #error
 print("Random forest regression error:",np.sqrt(mean_squared_error(ytest,result)))
 print("linear regression error:",np.sqrt(mean_squared_error(ytest,result3)))
 print("Decision Trees error:",np.sqrt(mean_squared_error(ytest,result2)))
+print ("gradientBoostingRegressor:",np.sqrt(mean_squared_error(ytest,gradientBoostingRegressorResult)))
 
 
 
 #output
-#Random forest regression error: 2.07865245117
-#linear regression error: 2.17697609745
-#lDecision Trees error: 2.83666189697
-
-
-
+#Random forest regression error: 2.04120748873
+#linear regression error: 2.06632655797
+#Decision Trees error: 2.6786120086
+#gradientBoostingRegressor error: 2.01407899865
 
 
 
